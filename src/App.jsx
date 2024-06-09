6294f44ad7e7d511c7ba23e9c35b4f3c1af50e74
Gopi2923 <dgopidhikshan@gmail.com>
2024-06-09 23:18:51 +0530

    learned props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button/Button"
 import Card from "./Card"
-
+import Student from "./Student"
 
 function App() {
 return (
@@ -8,6 +8,11 @@ return (
   <Card />
   <Card />
   <Button />
+  <Student name="Gopi" age={28} isStudent={true}/>
+  <Student name="Dhikshan" age={25} isStudent={false}/>
+  <Student name="John" age={21} isStudent={false}/>
+  <Student name="Doe" age={19} isStudent={true}/>
+  <Student />
  </>
 ) 
 }
---
+++ src/Student.jsx
@@ -0,0 +1,24 @@
+import propType from 'prop-types'
+
+const Student = (props) => {
+  return (
+    <div className="student">
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+      <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+    </div>
+  )
+}
+
+Student.propType = {
+    name: propType.string,
+    age: propType.number,
+    isStudent: propType.bool
+}
+
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    isStudent: false
+}
+export default Student
