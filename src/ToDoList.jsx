8131ac9d4e82bdf59a54abcea8bf813efbae2620
Gopi2923 <dgopidhikshan@gmail.com>
2024-06-19 21:01:59 +0530

    Format changes.

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -9,31 +9,37 @@ const ToDoList = () => {
   }
 
   function addTask() {
-    if(newTask.trim() !== ""){
-    setTasks(t => [...t, newTask])
-    setNewTask("")
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
   }
-}
 
   function deleteTask(index) {
-     const updatedTasks = tasks.filter((_, i) => i !== index)
-     setTasks(updatedTasks)
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
   }
 
   function moveTaskUp(index) {
-     if(index > 0) {
-        const updatedTasks = [...tasks];
-        [updatedTasks[index], updatedTasks[index -1]] = [updatedTasks[index -1], updatedTasks [index]];
-        setTasks(updatedTasks)
-     }
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
   }
 
   function moveTaskDown(index) {
-    if(index < tasks.length -1) {
-        const updatedTasks = [...tasks];
-        [updatedTasks[index], updatedTasks[index +1]] = [updatedTasks[index +1], updatedTasks [index]];
-        setTasks(updatedTasks)
-     }
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
   }
   return (
     <div className="todo-list">
