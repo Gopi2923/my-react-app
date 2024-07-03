fdcc219dc2bdaad781ba64430c24879e485baa3b
Gopi2923 <dgopidhikshan@gmail.com>
2024-07-03 19:02:05 +0530

    Format changes.

--- src/Useefffect.jsx
+++ src/Useefffect.jsx
@@ -1,62 +1,55 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 const Useefffect = () => {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("green");
 
-    const [count, setCount] = useState(0);
-    const [color, setColor] = useState('green');
-    
-    const [width, setWidth] = useState(window.innerWidth);
-    const [height, setHeight] = useState(window.innerHeight);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
 
-  
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
 
-    const handleResize = () => {
-        setWidth(window.innerWidth);
-        setHeight(window.innerHeight)
-    }
-
- useEffect(() => {
+  useEffect(() => {
     window.addEventListener("resize", handleResize);
-    console.log("EVENT LISTENER ADDED")
+    console.log("EVENT LISTENER ADDED");
 
     return () => {
-       window.removeEventListener("resize", handleResize)
-    console.log("EVENT LISTENER REMOVED")
+      window.removeEventListener("resize", handleResize);
+      console.log("EVENT LISTENER REMOVED");
+    };
+  }, []);
 
-    }
- }, [])   
+  useEffect(() => {
+    document.title = `Size: ${width} X ${height}`;
+  }, [width, height]);
 
- useEffect(() => {
-    document.title = `Size: ${width} X ${height}`
- }, [width, height])
-    
-   
+  useEffect(() => {
+    document.title = `Count: ${count} ${color}`;
+  }, [count, color]);
 
-    useEffect(() => {
-        document.title = `Count: ${count} ${color}` 
-    }, [count, color])
-
-    const handlebtn = () => {
-        setCount(c => c + 1)
-    }
-    const subtract = () => {
-        setCount(c => c - 1)
-    }
-    const changeColor = () => {
-        setColor(c=> c === 'green' ? "red" : 'green')
-    } 
+  const handlebtn = () => {
+    setCount((c) => c + 1);
+  };
+  const subtract = () => {
+    setCount((c) => c - 1);
+  };
+  const changeColor = () => {
+    setColor((c) => (c === "green" ? "red" : "green"));
+  };
   return (
     <div>
+      <p style={{ color: color }}>Count: {count}</p>
+      <button onClick={handlebtn}>Add</button>
+      <button onClick={subtract}>Subtract</button>
+      <button onClick={changeColor}>Change color</button>
 
-        <p style={{color: color}}>Count: {count}</p>
-        <button onClick={handlebtn}>Add</button>
-        <button onClick={subtract}>Subtract</button>
-        <button onClick={changeColor}>Change color</button>
-
-        <p>Width: {width}</p>
-        <p>Height: {height}</p>
+      <p>Width: {width}</p>
+      <p>Height: {height}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Useefffect
+export default Useefffect;
